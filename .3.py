f951cba7480f052842c6fab20d8954a4732fa54c
jayalaxmipatil0901-cell <jayalaxmipatil0901@gmail.com>
2026-03-20 12:23:19 +0530

    Add files via upload

---
+++ .3.py
@@ -0,0 +1,12 @@
+#creating two sets
+set1=set()
+set2=set()
+#adding elements to set1
+for i in range(1,8):
+    set1.add(i)
+ #adding element ti set2
+for i in range(1,8):
+    set2.add(i)
+print("set1=",set1)
+print("set2=",set2)
+print("\n")
